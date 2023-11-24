8087e178d656f1ce65cdcda8eb5eb6dff6eabaca
Joao Vinicius <joaoviniciuslameuteixeira@gmail.com>
2023-11-24 10:31:42 -0300

    4 Patas Shop

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | 4 Patas shop</title>
+    <!--Bootstrap Css-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!--Rodape Css -->
+
+    <link rel="stylesheet" href="rodape.css">
+
+    <!--Navbar Css-->
+    <link rel="stylesheet" href="navbar-home.css">
+    <link rel="shortcut icon" href="/asset/images/WhatsApp-Image-2023-10-06-at-10.49.13.ico" type="">
+    <!--Jquery Script-->
+    <script src="js/jquery-3.7.1.js"></script>
+</head>
+<body>
+    
+    
+    <main class="main">
+
+      <header>
+        <nav>
+            <a class="logo" href="/">4 Patas</a>
+            <div class="mobile-menu">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+            <ul class="nav-list">
+                <li><a href="/">Inicio</a></li>
+                <li><a href="/">Sobre</a></li>
+                <li><a href="/">Projetos</a></li>
+                <li><a href="/">Contatos</a></li>  
+           </ul>
+           
+           <div class="login-button">
+                <a href="entrar.php" class= "btn btn-primary">Entrar</a>
+               </div>
+           </div>
+        </nav>
+    </header>
+
+    <div class="text-center">
+  <img src="INICIO (1).png" class="rounded" alt="...">
+</div>
+
+      
+        </a>
+  
+    </button>
+  </div>
+  
+  <div class="swiffy-slider">
+    <ul class="slider-container">
+        <li><img src="posts instagram1.png" style="max-width: 30%;height: auto;"></li>
+        <li><img src="posts instagram2.png" style="max-width: 30%;height: auto;"></li>
+        <li><img src="cachorro posturado2.png" style="max-width: 30%;height: auto;"></li>
+    </ul>
+
+    <button type="button" class="slider-nav"></button>
+    <button type="button" class="slider-nav slider-nav-next"></button>
+
+    <div class="slider-indicators">
+        <button class="active"></button>
+        <button></button>
+        <button></button>
+    </div>
+</div>
+  
+<img src="recomendados.png" alt="">
+  </div>
+</div>
+
+
+  </div>
+</div>
+</div>
+
+<footer class="footer">
+    <div class="waves">
+      <div class="wave" id="wave1"></div>
+      <div class="wave" id="wave2"></div>
+      <div class="wave" id="wave3"></div>
+      <div class="wave" id="wave4"></div>
+    </div>
+    <ul class="social-icon">
+     
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-twitter"></ion-icon>
+        </a></li>
+    
+      <li class="social-icon__item"><a class="social-icon__link" href="https://instagram.com/_4patasshop_?igshid=OGQ5ZDc2ODk2ZA==">
+          <ion-icon name="logo-instagram"></ion-icon>
+        </a></li>
+    </ul>
+    <ul class="menu">
+      <li class="menu__item"><a class="menu__link" href="index.php">Inicio</a></li>
+
+      
+      <li class="menu__item"><a class="menu__link" href="Equipe.php">Equipe</a></li>
+     
+
+    </ul>
+    <a  href=>Precisa de ajuda? Entre em contato conosco ->
+        E-mail: 4pataspetshop@gmail.com ou Telefone: (13)99706-8478
+    </a>
+</li><p>&copy;2023 4 Patas   -   Pet Shop</p>
+  </footer>
+    </main>
+        <script src="mobile-navbar.js"></script>
+</body>
+</html>
